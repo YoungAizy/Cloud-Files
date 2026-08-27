5dc1543384cf64066e7d1e402cff5898378a605b
youngAizy <ayandamarotya@gmail.com>
2026-08-27 17:34:31 +0200

    add post route to download a file given a url payload

---
+++ app/main.py
@@ -0,0 +1,58 @@
+import os
+import uuid
+import httpx
+import aiofiles
+from fastapi import FastAPI, HTTPException
+
+from dotenv import load_dotenv
+
+from app.models import DownloadRequest
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+@app.post("/downloads")
+async def download_file(request: DownloadRequest):
+    try:
+        async with httpx.AsyncClient(
+            follow_redirects=True,
+            timeout=300
+        ) as client:
+
+            response = await client.get(str(request.url))
+
+            response.raise_for_status()
+
+        filename = request.url.path.split("/")[-1]
+
+        if not filename:
+            filename = f"{uuid.uuid4()}"
+
+        object_name = f"./{filename}"
+        if response.status_code == 200:
+            await save_file(
+                response,
+                object_name
+            )
+
+        return {
+            "success": True,
+            "object_name": object_name
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
+
+
+async def save_file(response,name):
+    async with aiofiles.open(name, "wb") as out_file:
+        async for chunk in response.aiter_bytes(chunk_size=1024 * 64): # 64KB chunks
+            await out_file.write(chunk)
