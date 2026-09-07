49e9eff1b02ab395215b621fa6bfdf762adba074
youngAizy <ayandamarotya@gmail.com>
2026-09-07 14:56:25 +0200

    create an unimplemented email sender file for when download completes

---
+++ app/email_sender.py
@@ -0,0 +1,8 @@
+
+subject = "Drive-Drop Upload Successful/Failed"
+
+def _retrieve_email(access_toke: str) -> str:
+    pass
+
+def send_completion_email(access_token: str, is_successful: bool, error_detail = None):
+    pass
