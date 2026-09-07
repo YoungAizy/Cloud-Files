38f877c34a5fefa73d9d29d8cddb6642a9c046b8
youngAizy <ayandamarotya@gmail.com>
2026-09-07 15:23:46 +0200

    rename error_detail to payload_data

--- app/email_sender.py
+++ app/email_sender.py
@@ -4,5 +4,5 @@ subject = "Drive-Drop Upload Successful/Failed"
 def _retrieve_email(access_toke: str) -> str:
     pass
 
-def send_completion_email(access_token: str, is_successful: bool, error_detail = None):
+def send_completion_email(access_token: str, is_successful: bool, payload_data):
     pass
